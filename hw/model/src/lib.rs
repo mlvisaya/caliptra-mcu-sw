6b3141f46e4f5879f17e5dcc0224e8e42bb17f9f
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-03-18 09:21:45 -0700

    [xtask/infra]: Add parallel build option (#1085)
    Support building firmware in multiple directories to saturate the
    builder CPU & speed up firmware build step.
    
    (cherry picked from commit ddc2f0f96a6cbc6c7ef56b24050c708906969c51)

--- hw/model/src/lib.rs
+++ hw/model/src/lib.rs
@@ -987,6 +987,7 @@ mod tests {
             let rom_file = caliptra_mcu_builder::test_rom_build(
                 Some(platform()),
                 &firmware::hw_model_tests::MAILBOX_RESPONDER,
+                None,
             )?;
             std::fs::read(&rom_file)?
         };
